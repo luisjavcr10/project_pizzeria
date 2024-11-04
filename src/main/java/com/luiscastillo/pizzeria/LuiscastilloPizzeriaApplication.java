b3c052ff547d16c9ee26ca83b064496f4718094c
Luis Javier Castillo Rabanal <luisjaviercastillorabanal@MacBook-Air-de-Luis.local>
2024-11-04 13:06:03 -0500

    First configuration for data source

---
+++ src/main/java/com/luiscastillo/pizzeria/LuiscastilloPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.luiscastillo.pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuiscastilloPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuiscastilloPizzeriaApplication.class, args);
+	}
+
+}
